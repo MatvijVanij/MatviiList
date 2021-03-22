711cc3f40f16255625bad9b62e93f4ea95fdeae3
MatvijVanij <matvijiva1@gmail.com>
2021-03-22 19:25:54 +0200

    begin LinkedList

---
+++ MatviiList/LinkedList.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatviiList
+{
+    class LinkedList
+    {
+        public int Length { get; private set; }
+        public int this[int index]
+        {
+            get
+            {
+                if (index < 0 && index > Length)
+                {
+                   return GetNodeByIndex(index).Value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException("");
+                }
+            }
+            set
+            {
+                if (index < 0 && index > Length)
+                {
+                    GetNodeByIndex(index).Value = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException("");
+                }
+            }
+        }
+        private Node _root;
+        private Node _tail;
+        public LinkedList()
+        {
+            Length = 0;
+            _root = null;
+            _tail = null;
+        }
+        public LinkedList(int value)
+        {
+            Length = 1;
+            _root = new Node(value);
+            _tail = _root;
+        }
+
+        public LinkedList(int[] values)
+        {
+            if (values is null)
+            {
+                if (values.Length != 0)
+                {
+                    Length = values.Length;
+                    _root = new Node(values[0]);
+                    _root = _tail;
+
+                    for (int i = 1; i < values.Length; i++)
+                    {
+                        _tail.Next = new Node(values[i]);
+                        _tail = _tail.Next;
+                    }
+                }
+                else
+                {
+                    _root = null;
+                    _tail = null;
+                }
+            }
+            throw new NullReferenceException("no value in list");
+        }
+
+        private Node GetNodeByIndex(int index)
+        {
+            Node current = _root;
+            for (int i = 1; i <= index; i++)
+            {
+                current = current.Next;
+            }
+            return current;
+        }
+
+        public void AddLast(int value)
+        {
+            Length++;
+            _tail.Next = new Node(value);
+            _tail = _tail.Next;
+        }
+        public void AddFirst(int value)
+        {
+            Length++;
+            _root.Next = new Node(value); 
+            _root = _root.Next;
+        }
+    }
+}
---
+++ MatviiListTests/LinkedListTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatviiListTests
+{
+    class LinkedListTests
+    {
+    }
+}
